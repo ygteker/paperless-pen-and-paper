9b5fd74db4fd3580f5412376a943a4b66c0f5856
Llewe <ll.hochhauser@campus.lmu.de>
2021-12-16 14:21:16 +0100

    - added SwaggerConfiguration.kt to enable authentication with token

--- backend/src/main/kotlin/lmu/msp/backend/controller/HelloWorldController.kt
+++ backend/src/main/kotlin/lmu/msp/backend/controller/HelloWorldController.kt
@@ -1,5 +1,6 @@
 package lmu.msp.backend.controller
 
+import io.swagger.v3.oas.annotations.security.SecurityRequirement
 import org.springframework.security.core.Authentication
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
@@ -18,6 +19,7 @@ class HelloWorldController {
     }
 
     @GetMapping("/authentication")
+    @SecurityRequirement(name = "bearer-key")
     fun getHelloWorldWithAuthentication(authentication: Authentication): String {
         return "Hello ${authentication.name}!"
     }
