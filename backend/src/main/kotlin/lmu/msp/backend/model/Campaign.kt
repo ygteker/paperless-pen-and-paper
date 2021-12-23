f051f6fa7be41ead9ac7a48235cebe48f1c0a074
Llewe <ll.hochhauser@campus.lmu.de>
2021-12-23 15:57:40 +0100

    - updated Campaign and User model - added tests for campaign model

--- backend/src/main/kotlin/lmu/msp/backend/model/Campaign.kt
+++ backend/src/main/kotlin/lmu/msp/backend/model/Campaign.kt
@@ -3,16 +3,27 @@ package lmu.msp.backend.model
 import javax.persistence.*
 
 @Entity
-class Campaign(
-    @ManyToOne(fetch = FetchType.LAZY)
+final class Campaign(
+    @JoinColumn(nullable = false)
+    @ManyToOne(fetch = FetchType.EAGER)
     val owner: User,
     @Column(nullable = false, length = 45)
     val title: String,
-    @OneToMany(mappedBy = "campaign", cascade = [CascadeType.ALL], orphanRemoval = true)
+    @OneToMany(mappedBy = "campaign", cascade = [CascadeType.ALL], fetch = FetchType.EAGER, orphanRemoval = true)
     val campaignMembers: List<CampaignMember> = emptyList()
 ) {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    @Column(name = "id", nullable = false)
+    @Column(nullable = false)
     val id: Long = 0
+
+    init {
+        owner.campaignOwner.add(this)
+    }
+
+    override fun toString(): String {
+        return "Campaign(owner=${owner.id}, title='$title', campaignMembers=$campaignMembers, id=$id)"
+    }
+
+
 }
--- backend/src/main/kotlin/lmu/msp/backend/model/User.kt
+++ backend/src/main/kotlin/lmu/msp/backend/model/User.kt
@@ -9,15 +9,20 @@ class User(
     @JsonIgnore //frontend doesn't need this value.
     val auth0Id: String,
 
-    @OneToMany(mappedBy = "owner", cascade = [CascadeType.ALL], orphanRemoval = true)
-    val campaignOwner: List<Campaign> = emptyList(),
+    @OneToMany(mappedBy = "owner", cascade = [CascadeType.ALL], fetch = FetchType.EAGER, orphanRemoval = true)
+    val campaignOwner: MutableList<Campaign> = mutableListOf(),
 
     @OneToMany(mappedBy = "user", cascade = [CascadeType.ALL], orphanRemoval = true)
     val campaignMember: List<CampaignMember> = emptyList()
 ) {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    @Column(name = "id", nullable = false)
+    @Column(nullable = false)
     val id: Long = 0
+
+    override fun toString(): String {
+        return "User(auth0Id='$auth0Id', campaignOwner=$campaignOwner, campaignMember=$campaignMember, id=$id)"
+    }
+
 
 }
