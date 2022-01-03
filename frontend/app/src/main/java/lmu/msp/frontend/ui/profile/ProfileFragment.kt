880358b8348ac837d2d1452902c9785d4b52300a
vscheibe <62471491+vscheibe@users.noreply.github.com>
2022-01-03 18:43:15 +0100

    Simplify ProfileFragment.kt

--- frontend/app/src/main/java/lmu/msp/frontend/ui/profile/ProfileFragment.kt
+++ frontend/app/src/main/java/lmu/msp/frontend/ui/profile/ProfileFragment.kt
@@ -8,37 +8,16 @@ import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import lmu.msp.frontend.R
 import lmu.msp.frontend.databinding.FragmentProfileBinding
 
 class ProfileFragment : Fragment() {
-
-    private lateinit var profileViewModel: ProfileViewModel
-    private var _binding: FragmentProfileBinding? = null
-
-    // This property is only valid between onCreateView and
-    // onDestroyView.
-    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        profileViewModel =
-            ViewModelProvider(this).get(ProfileViewModel::class.java)
-
-        _binding = FragmentProfileBinding.inflate(inflater, container, false)
-        val root: View = binding.root
-
-        val textView: TextView = binding.textProfile
-        profileViewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
-        })
-        return root
-    }
-
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
+        return inflater.inflate(R.layout.fragment_home, container, false)
     }
 }
