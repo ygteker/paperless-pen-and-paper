8cc67336e9a812246c929caf6f97241b6d249bf4
Yagiz Gunes Teker <gunes.teker@gmail.com>
2022-01-09 17:55:04 +0100

    Auth0 login implemented excess view elements removed theme for LoginActivity.kt adjusted

--- frontend/app/src/main/java/lmu/msp/frontend/LoginActivity.kt
+++ frontend/app/src/main/java/lmu/msp/frontend/LoginActivity.kt
@@ -21,7 +21,6 @@ class LoginActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityLoginBinding
     private lateinit var account: Auth0
-    private lateinit var accessToken: String
 
     private lateinit var auth: AuthenticationAPIClient
 
@@ -32,84 +31,33 @@ class LoginActivity : AppCompatActivity() {
         setContentView(view)
 
         account = Auth0(
-            "fO2ATpY1mfFrutuO9HLpiiF80qQRSJiH",
-            "dev-paperlesspenandpaper.eu.auth0.com"
+            getString(R.string.com_auth0_client_id),
+            getString(R.string.com_auth0_domain)
         )
 
         auth = AuthenticationAPIClient(account)
 
         val loginButton = binding.loginButton
-        val email = binding.email.text.toString()
-        val password = binding.password.text.toString()
-        val databaseConnection: String = ""
 
         loginButton.setOnClickListener {
-            // Toast.makeText(this@LoginActivity, "Login", Toast.LENGTH_SHORT).show()
-
-            // Login method
-        //    login(email, password, databaseConnection)
-            intent = Intent(this, HomeActivity::class.java)
-            startActivity(intent)
-
-            // loginWithBrowser()
-
+            loginWithBrowser()
         }
-
-        val registerButton = binding.registerButton
-
-        registerButton.setOnClickListener {
-            // Toast.makeText(this@LoginActivity, "Register", Toast.LENGTH_SHORT).show()
-            val intent = Intent(this, RegisterActivity::class.java)
-            startActivity(intent)
-        }
-    }
-
-    private fun login(email: String, password: String, databaseConnection: String) {
-        auth
-            .login(email, password)
-            .setConnection(databaseConnection)
-            .start(object: Callback<Credentials, AuthenticationException> {
-                override fun onFailure(exception: AuthenticationException) {
-                    //TODO(Handle failure)
-                }
-
-                override fun onSuccess(credentials: Credentials) {
-                    //TODO(redirect to HomeActivity or ProfileActivity)
-                }
-            })
     }
 
     private fun loginWithBrowser() {
         WebAuthProvider.login(account)
             .withScheme("android")
+            .withScope("openid profile email")
             .start(this, object: Callback<Credentials, AuthenticationException> {
-                override fun onFailure(exception: AuthenticationException) {
-                    //TODO(handle exception)
+                override fun onFailure(error: AuthenticationException) {
+                    error.printStackTrace()
                 }
 
-                override fun onSuccess(credentials: Credentials) {
-                    accessToken = credentials.accessToken
-                    print(accessToken)
-                }
-            })
-    }
-
-    private fun showUserInformation(accessToken: String) {
-        var client = AuthenticationAPIClient(account)
-
-        // With the access token, call `userInfo` and get the profile from Auth0.
-        client.userInfo(accessToken)
-            .start(object : Callback<UserProfile, AuthenticationException> {
-                override fun onFailure(exception: AuthenticationException) {
-                    // Something went wrong!
-                }
-
-                override fun onSuccess(profile: UserProfile) {
-                    // We have the user's profile!
-                    val email = profile.email
-                    val name = profile.name
-
-                    print(email + name)
+                override fun onSuccess(result: Credentials) {
+                    val accessToken = result.accessToken
+                    intent = Intent(this@LoginActivity, HomeActivity::class.java)
+                    intent.putExtra("access_token", accessToken)
+                    startActivity(intent)
                 }
             })
     }
