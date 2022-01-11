4fb170269a47bdbb822fdc0470a4343530ddf365
vscheibe <62471491+vscheibe@users.noreply.github.com>
2022-01-11 15:27:16 +0100

    Added new Animated Dice Test classes. Added Add campaigns button to home screen

--- frontend/app/src/main/java/lmu/msp/frontend/ui/campaign/CampaignFragment.kt
+++ frontend/app/src/main/java/lmu/msp/frontend/ui/campaign/CampaignFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
@@ -21,12 +22,16 @@ class CampaignFragment : Fragment() {
     private lateinit var newArrayList: ArrayList<campaigns>
     private lateinit var titleStrings: Array<String>
     private lateinit var campaignIds: Array<String>
+    private lateinit var addCampaignButton : Button
+    private lateinit var campaignAdapter : CampaignAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+
+
 
         titleStrings = arrayOf(
             "Curse of Strahd",
@@ -39,20 +44,31 @@ class CampaignFragment : Fragment() {
             "000003"
         )
         val view = inflater.inflate(R.layout.fragment_campaign, container, false)
+        addCampaignButton = view.findViewById(R.id.testAddButton)
 
         newRecyclerView = view.findViewById(R.id.recyclerView)
         val horizontalLayoutManager =
             LinearLayoutManager(context, LinearLayoutManager.HORIZONTAL, false)
         newRecyclerView.layoutManager = horizontalLayoutManager
         newRecyclerView.setHasFixedSize(true)
+        newArrayList = arrayListOf<campaigns>()
 
+        campaignAdapter = CampaignAdapter(newArrayList)
+        newRecyclerView.adapter = campaignAdapter
         val helper = LinearSnapHelper()
         helper.attachToRecyclerView(newRecyclerView)
 
-        newArrayList = arrayListOf<campaigns>()
         fillCampaigns()
 
+        addCampaignButton.setOnClickListener { addCampaign() }
+
         return view
+    }
+
+    private fun addCampaign() {
+       var data = campaigns("Rime of the Frostmaiden", "00004")
+        newArrayList.add(data)
+        newRecyclerView.adapter?.notifyDataSetChanged()
     }
 
     private fun fillCampaigns() {
@@ -61,6 +77,7 @@ class CampaignFragment : Fragment() {
             val data = campaigns(titleStrings[i], campaignIds[i])
             newArrayList.add(data)
         }
-        newRecyclerView.adapter = CampaignAdapter(newArrayList)
+
+        campaignAdapter.notifyDataSetChanged()
     }
 }
--- frontend/app/src/main/java/lmu/msp/frontend/ui/campaign/ToolsFragment.kt
+++ frontend/app/src/main/java/lmu/msp/frontend/ui/campaign/ToolsFragment.kt
@@ -23,6 +23,7 @@ class ToolsFragment : Fragment() {
         arrayList.add("Map")
         arrayList.add("Roll Dice")
         arrayList.add("Chat")
+        arrayList.add("Roll animated Dice")
         val arrayAdapter = ArrayAdapter(view.context, android.R.layout.simple_list_item_1, arrayList)
         toolsList.adapter = arrayAdapter
         toolsList.setOnItemClickListener { _, view, position, id ->
@@ -31,6 +32,7 @@ class ToolsFragment : Fragment() {
                 0 -> fragmentManager.replace(R.id.fragment, MapFragment()).addToBackStack(null)
                 1 -> fragmentManager.replace(R.id.fragment, DiceFragment()).addToBackStack(null)
                 2 -> fragmentManager.replace(R.id.fragment, ChatFragment()).addToBackStack(null)
+                3 -> fragmentManager.replace(R.id.fragment, DiceFragmentAnimated()).addToBackStack(null)
             }
             fragmentManager.commit()
         }
