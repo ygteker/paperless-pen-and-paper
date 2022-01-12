11bcc11cbee93fbb9a561eb6664db79bb4d52384
vscheibe <62471491+vscheibe@users.noreply.github.com>
2022-01-12 17:36:11 +0100

    Implemented all dice animations and images. Dice feature now working. Missing colors of d12 and d20.

--- frontend/app/src/main/java/lmu/msp/frontend/diceRolling/DiceFragmentAnimated.kt
+++ frontend/app/src/main/java/lmu/msp/frontend/diceRolling/DiceFragmentAnimated.kt
@@ -1,13 +1,12 @@
 package lmu.msp.frontend.diceRolling
 
+import android.annotation.SuppressLint
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.view.animation.AnimationUtils
 import android.widget.Button
-import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 import androidx.fragment.app.Fragment
@@ -25,7 +24,6 @@ class DiceFragmentAnimated : Fragment() {
     private lateinit var addButtonD12: Button
     private lateinit var addButtonD20: Button
     private lateinit var rollAllDice: Button
-    private lateinit var testButton: Button
 
     private lateinit var resultTextView: TextView
 
@@ -60,13 +58,11 @@ class DiceFragmentAnimated : Fragment() {
         addButtonD12.setOnClickListener { addDice(12) }
         addButtonD20.setOnClickListener { addDice(20) }
         rollAllDice.setOnClickListener { rollAllDiceFunction() }
-        testButton.setOnClickListener { animateDice() }
-
     }
 
     private fun animateDice() {
 
-        var shake = AnimationUtils.loadAnimation(context, R.anim.shake3)
+        val shake = AnimationUtils.loadAnimation(context, R.anim.shake3)
         var i = 0
         while (i <= diceArrayList.size) {
             rollDiceRecyclerView.findViewHolderForAdapterPosition(i)?.itemView?.startAnimation(shake)
@@ -79,27 +75,36 @@ class DiceFragmentAnimated : Fragment() {
         animateDice()
         var i = 0
         while (i < diceArrayList.size) {
-            when (diceArrayList.get(i).sides) {
+            when (diceArrayList[i].sides) {
                 4 -> {
-                    resultsArray.add(Random.nextInt(4) + 1)
+                    val random = Random.nextInt(4) + 1
+                    resultsArray.add(random)
+                    changePicture(4, random, i)
                 }
                 6 -> {
                     val random = Random.nextInt(6) + 1
                     resultsArray.add(random)
-
                     changePicture(6, random, i)
                 }
                 8 -> {
-                    resultsArray.add(Random.nextInt(8) + 1)
+                    val random = Random.nextInt(8) + 1
+                    resultsArray.add(random)
+                    changePicture(8, random, i)
                 }
                 10 -> {
-                    resultsArray.add(Random.nextInt(10) + 1)
+                    val random = Random.nextInt(10) + 1
+                    resultsArray.add(random)
+                    changePicture(10, random, i)
                 }
                 12 -> {
-                    resultsArray.add(Random.nextInt(12) + 1)
+                    val random = Random.nextInt(12) + 1
+                    resultsArray.add(random)
+                    changePicture(12, random, i)
                 }
                 20 -> {
-                    resultsArray.add(Random.nextInt(20) + 1)
+                    val random = Random.nextInt(20) + 1
+                    resultsArray.add(random)
+                    changePicture(20, random, i)
                 }
             }
             i++
@@ -109,24 +114,94 @@ class DiceFragmentAnimated : Fragment() {
     }
 
     private fun changePicture(sides: Int, random: Int, index: Int) {
-
         when (sides) {
+            4 -> {
+                when (random) {
+                    1 -> diceArrayList[index].imageId = R.drawable.d4_1
+                    2 -> diceArrayList[index].imageId = R.drawable.d4_2
+                    3 -> diceArrayList[index].imageId = R.drawable.d4_3
+                    4 -> diceArrayList[index].imageId = R.drawable.d4_4
+                }
+            }
             6 -> {
                 when (random) {
-                    1 -> diceArrayList.get(index).imageId = R.drawable.d6_one
-                    2 -> diceArrayList.get(index).imageId = R.drawable.d6_two
-                    3 -> diceArrayList.get(index).imageId = R.drawable.d6_three
-                    4 -> diceArrayList.get(index).imageId = R.drawable.d6_four
-                    5 -> diceArrayList.get(index).imageId = R.drawable.d6_five
-                    6 -> diceArrayList.get(index).imageId = R.drawable.d6_six
+                    1 -> diceArrayList[index].imageId = R.drawable.d6_1
+                    2 -> diceArrayList[index].imageId = R.drawable.d6_2
+                    3 -> diceArrayList[index].imageId = R.drawable.d6_3
+                    4 -> diceArrayList[index].imageId = R.drawable.d6_4
+                    5 -> diceArrayList[index].imageId = R.drawable.d6_5
+                    6 -> diceArrayList[index].imageId = R.drawable.d6_6
                 }
-
+            }
+            8 -> {
+                when (random) {
+                    1 -> diceArrayList[index].imageId = R.drawable.d8_1
+                    2 -> diceArrayList[index].imageId = R.drawable.d8_2
+                    3 -> diceArrayList[index].imageId = R.drawable.d8_3
+                    4 -> diceArrayList[index].imageId = R.drawable.d8_4
+                    5 -> diceArrayList[index].imageId = R.drawable.d8_5
+                    6 -> diceArrayList[index].imageId = R.drawable.d8_6
+                    7 -> diceArrayList[index].imageId = R.drawable.d8_7
+                    8 -> diceArrayList[index].imageId = R.drawable.d8_8
+                }
+            }
+            10 -> {
+                when (random) {
+                    1 -> diceArrayList[index].imageId = R.drawable.d10_1
+                    2 -> diceArrayList[index].imageId = R.drawable.d10_2
+                    3 -> diceArrayList[index].imageId = R.drawable.d10_3
+                    4 -> diceArrayList[index].imageId = R.drawable.d10_4
+                    5 -> diceArrayList[index].imageId = R.drawable.d10_5
+                    6 -> diceArrayList[index].imageId = R.drawable.d10_6
+                    7 -> diceArrayList[index].imageId = R.drawable.d10_7
+                    8 -> diceArrayList[index].imageId = R.drawable.d10_8
+                    9 -> diceArrayList[index].imageId = R.drawable.d10_9
+                    10 -> diceArrayList[index].imageId = R.drawable.d10_10
+                }
+            }
+            12 -> {
+                when (random) {
+                    1 -> diceArrayList[index].imageId = R.drawable.d12_1
+                    2 -> diceArrayList[index].imageId = R.drawable.d12_2
+                    3 -> diceArrayList[index].imageId = R.drawable.d12_3
+                    4 -> diceArrayList[index].imageId = R.drawable.d12_4
+                    5 -> diceArrayList[index].imageId = R.drawable.d12_5
+                    6 -> diceArrayList[index].imageId = R.drawable.d12_6
+                    7 -> diceArrayList[index].imageId = R.drawable.d12_7
+                    8 -> diceArrayList[index].imageId = R.drawable.d12_8
+                    9 -> diceArrayList[index].imageId = R.drawable.d12_9
+                    10 -> diceArrayList[index].imageId = R.drawable.d12_10
+                    11 -> diceArrayList[index].imageId = R.drawable.d12_11
+                    12 -> diceArrayList[index].imageId = R.drawable.d12_12
+                }
+            }
+            20 -> {
+                when (random) {
+                    1 -> diceArrayList[index].imageId = R.drawable.d20_1
+                    2 -> diceArrayList[index].imageId = R.drawable.d20_2
+                    3 -> diceArrayList[index].imageId = R.drawable.d20_3
+                    4 -> diceArrayList[index].imageId = R.drawable.d20_4
+                    5 -> diceArrayList[index].imageId = R.drawable.d20_5
+                    6 -> diceArrayList[index].imageId = R.drawable.d20_6
+                    7 -> diceArrayList[index].imageId = R.drawable.d20_7
+                    8 -> diceArrayList[index].imageId = R.drawable.d20_8
+                    9 -> diceArrayList[index].imageId = R.drawable.d20_9
+                    10 -> diceArrayList[index].imageId = R.drawable.d20_10
+                    11 -> diceArrayList[index].imageId = R.drawable.d20_11
+                    12 -> diceArrayList[index].imageId = R.drawable.d20_12
+                    13 -> diceArrayList[index].imageId = R.drawable.d20_13
+                    14 -> diceArrayList[index].imageId = R.drawable.d20_14
+                    15 -> diceArrayList[index].imageId = R.drawable.d20_15
+                    16 -> diceArrayList[index].imageId = R.drawable.d20_16
+                    17 -> diceArrayList[index].imageId = R.drawable.d20_17
+                    18 -> diceArrayList[index].imageId = R.drawable.d20_18
+                    19 -> diceArrayList[index].imageId = R.drawable.d20_19
+                    20 -> diceArrayList[index].imageId = R.drawable.d20_20
+                }
             }
 
         }
-          diceRollingAdapter.notifyItemChanged(index)
-      //  diceRollingAdapter.notifyDataSetChanged()
-
+        diceRollingAdapter.notifyItemChanged(index)
     }
 
     private fun displayResult() {
@@ -138,39 +213,40 @@ class DiceFragmentAnimated : Fragment() {
             added += resultsArray[i]
             i++
         }
-        val output2 = "Result: " + output + ", Added: " + added.toString()
-        resultTextView.setText(output2)
+        val output2 = "Result: $output, Added: $added"
+        resultTextView.text = output2
     }
 
+    @SuppressLint("NotifyDataSetChanged")
     private fun addDice(sides: Int) {
-        if (diceArrayList.size >= 28) {
+        if (diceArrayList.size >= 24) {
             Toast.makeText(context, "Cannot add any more dice", Toast.LENGTH_SHORT).show()
         } else {
             val data: diceData
             when (sides) {
                 4 -> {
-                    data = diceData(sides, R.drawable.d6_two)
+                    data = diceData(sides, R.drawable.d4_all)
                     diceArrayList.add(data)
                 }
                 6 -> {
-                    data = diceData(sides, R.drawable.d6_one)
+                    data = diceData(sides, R.drawable.d6_1)
                     diceArrayList.add(data)
                 }
                 8 -> {
-                    data = diceData(sides, R.drawable.d6_three)
+                    data = diceData(sides, R.drawable.d8_all)
                     diceArrayList.add(data)
 
                 }
                 10 -> {
-                    data = diceData(sides, R.drawable.d6_four)
+                    data = diceData(sides, R.drawable.d10_all)
                     diceArrayList.add(data)
                 }
                 12 -> {
-                    data = diceData(sides, R.drawable.d6_five)
+                    data = diceData(sides, R.drawable.d12_all)
                     diceArrayList.add(data)
                 }
                 20 -> {
-                    data = diceData(sides, R.drawable.d6_six)
+                    data = diceData(sides, R.drawable.d20_all)
                     diceArrayList.add(data)
                 }
             }
@@ -186,7 +262,7 @@ class DiceFragmentAnimated : Fragment() {
         addButtonD12 = view.findViewById(R.id.addButtonD12)
         addButtonD20 = view.findViewById(R.id.addButtonD20)
         rollAllDice = view.findViewById(R.id.rollDiceButtonAll)
-        testButton = view.findViewById(R.id.testButton)
+
 
         resultTextView = view.findViewById(R.id.resultTextView)
 
@@ -195,48 +271,5 @@ class DiceFragmentAnimated : Fragment() {
         diceRollingAdapter = DiceRollingAdapter(diceArrayList)
         rollDiceRecyclerView.adapter = diceRollingAdapter
     }
-
-
-/*
-    private fun rotateDice() {
-        var i = Random.nextInt(6) +1
-        var shake = AnimationUtils.loadAnimation(context, R.anim.shake)
-        d6View.startAnimation(shake)
-     /*
-        d6View.animate().apply {
-            duration = 500
-           // rotationYBy(720f)
-            rotationBy(360f)
-        }
-      */
-        when (i) {
-            1 -> {
-                d6View.setImageResource(R.drawable.d6_one)
-                resultView.setText("Result: $i")
-            }
-            2 -> {
-                d6View.setImageResource(R.drawable.d6_two)
-                resultView.setText("Result: $i")
-            }
-            3 -> {
-                d6View.setImageResource(R.drawable.d6_three)
-                resultView.setText("Result: $i")
-            }
-            4 -> {
-                d6View.setImageResource(R.drawable.d6_four)
-                resultView.setText("Result: $i")
-            }
-            5 -> {
-                d6View.setImageResource(R.drawable.d6_five)
-                resultView.setText("Result: $i")
-            }
-            6 -> {
-                d6View.setImageResource(R.drawable.d6_six)
-                resultView.setText("Result: $i")
-            }
-        }
-    }
-
- */
 
 }
